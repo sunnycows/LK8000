83d3191de1505e486bc2cf70bd97514af794085a
Nicola Ferruzzi <nicola.ferruzzi@gmail.com>
2018-07-29 14:21:10 +0200

    Move to sdl 2.0.8 and change the way appdelegate executes lk8000 main

--- ios/LK8000i/AppDelegate.m
+++ ios/LK8000i/AppDelegate.m
@@ -8,7 +8,16 @@
 // libPNG requires: -DPNG_ARM_NEON_OPT=0
 
 #import <Objc/runtime.h>
-#import <SDL.h>
+
+extern "C" {
+    #include "../Pods/sdl2/include/SDL.h"
+    #include "../Pods/sdl2/include/SDL_events.h"
+    #include "../Pods/sdl2/src/SDL_internal.h"
+    #include "../Pods/sdl2/src/events/SDL_windowevents_c.h"
+    #include "../Pods/sdl2/src/events/SDL_events_c.h"
+    #include "../Pods/sdl2/src/video/SDL_sysvideo.h"
+}
+
 #import <Fabric/Fabric.h>
 #import <Crashlytics/Crashlytics.h>
 
@@ -20,41 +29,6 @@
 #include "Parser.h"
 
 extern NMEA_INFO GPS_INFO;
-
-@interface SDLUIKitDelegate (LK8000)
-@end
-
-// From so
-void SwizzleClassMethod(Class c, SEL orig, SEL newSel) {
-    Method origMethod = class_getClassMethod(c, orig);
-    Method newMethod = class_getClassMethod(c, newSel);
-    
-    c = object_getClass((id)c);
-    
-    if (class_addMethod(c, orig, method_getImplementation(newMethod), method_getTypeEncoding(newMethod))) {
-        class_replaceMethod(c, newSel, method_getImplementation(origMethod), method_getTypeEncoding(origMethod));
-    } else {
-        method_exchangeImplementations(origMethod, newMethod);
-    }
-}
-
-@implementation SDLUIKitDelegate (LK8000)
-
-+ (void)load {
-    if (self != [SDLUIKitDelegate class]) {
-        return;
-    }
-    
-    SwizzleClassMethod([SDLUIKitDelegate class],
-                       @selector(getAppDelegateClassName),
-                       @selector(getAppDelegateClassNameCustom));
-}
-
-+ (NSString *)getAppDelegateClassNameCustom {
-    return @"AppDelegate";
-}
-
-@end
 
 #pragma mark 
 
@@ -153,54 +127,59 @@ void SwizzleClassMethod(Class c, SEL orig, SEL newSel) {
     return nmea;
 }
 
+static void SDLCALL
+SDL_IdleTimerDisabledChanged(void *userdata, const char *name, const char *oldValue, const char *hint)
+{
+    BOOL disable = (hint && *hint != '0');
+    [UIApplication sharedApplication].idleTimerDisabled = disable;
+}
+
 - (bool)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     [Fabric with:@[[Crashlytics class]]];
 
-    static ArchiveUnzip *archive = [ArchiveUnzip new];
+    SDL_AddHintCallback(SDL_HINT_IDLE_TIMER_DISABLED, SDL_IdleTimerDisabledChanged, NULL);
 
-    __block BOOL unzip, locupdate, done;
+    UIScreen *screen = [UIScreen mainScreen];
+    self.window = [[UIWindow alloc]initWithFrame:screen.bounds];
+    UIViewController *uv = [[UIViewController alloc] init];
+    uv.view.backgroundColor = [UIColor yellowColor];
     
+    [self.window setRootViewController:uv];
+    [self.window makeKeyAndVisible];
+
+    static ArchiveUnzip *archive = [ArchiveUnzip new];
     [archive startDecompression:^(NSError *error) {
         // Wait for decompression; LK8000 requires a proper data folders in order
         // to initialize properly; unfortunately SDL-iOS delegate (subclassed by this class)
         // does not provide an hook to call the "SDL main" on demand.
         NSLog(@"Decompression done: %@", error);
-        unzip = YES;
-        
-        if (unzip && locupdate && !done) {
-            done = YES;
-            [super application:application didFinishLaunchingWithOptions:launchOptions];
-        }
+        [self performSelector:@selector(requestPosition) withObject:nil];
     }];
-    
-    UIScreen *screen = [UIScreen mainScreen];
-    self.uiWindow = [[UIWindow alloc]initWithFrame:screen.bounds];
-    self.uiWindow.backgroundColor = [UIColor redColor];
-    static UIViewController *uv = [[UIViewController alloc] init];
-    uv.view.backgroundColor = [UIColor yellowColor];
-    
-    [self.uiWindow setRootViewController:uv];
-    [self.uiWindow makeKeyAndVisible];
 
-    
+    return YES;
+}
+
+- (void)requestPosition{
+    __block BOOL canRunLK8000 = true;
+    __weak typeof(self) wself = self;
+
     [[LKCLHelper sharedInstance] requestLocationIfPossibleWithUI:YES
                                                            block:^bool(LKCLHelperStatus status, CLLocation *location, CMAltitudeData *altitude) {
-                                                               NSLog(@"%lu", (unsigned long)status);
-                                                               locupdate = YES;
-                                                               
-                                                               if (unzip && locupdate && !done) {
-                                                                   done = YES;
-                                                                   [super application:application didFinishLaunchingWithOptions:launchOptions];
-                                                                   return true;
+                                                               if (canRunLK8000) {
+                                                                   canRunLK8000 = false;
+                                                                   [wself performSelector:@selector(runLK8000) withObject:nil];
                                                                }
-                                                               
-                                                               if (done) {
-                                                                   [self sendLocation:location
-                                                                             altitude:altitude];
-                                                               }
-                                                               return true;
+                                                               [self sendLocation:location altitude:altitude];
+                                                               return TRUE;
                                                            }];
-    return YES;
+
+}
+
+- (void)runLK8000 {
+    SDL_SetMainReady();
+    SDL_iPhoneSetEventPump(SDL_TRUE);
+    LK8000_main(0, NULL);
+    SDL_iPhoneSetEventPump(SDL_FALSE);
 }
 
 - (void)sendLocation:(CLLocation *)location altitude:(CMAltitudeData *)altitude {
@@ -225,48 +204,87 @@ void SwizzleClassMethod(Class c, SEL orig, SEL newSel) {
     }
 }
 
-//#define DEBUG_INTEGRATION
-
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // SDL parent class manage this
-#ifndef DEBUG_INTEGRATION
-    [super applicationWillResignActive:application];
-#endif
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+    SDL_SendAppEvent(SDL_APP_TERMINATING);
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // SDL parent class manage this
-#ifndef DEBUG_INTEGRATION
-    [super applicationDidEnterBackground:application];
-#endif
+- (void)applicationDidReceiveMemoryWarning:(UIApplication *)application
+{
+    SDL_SendAppEvent(SDL_APP_LOWMEMORY);
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // SDL parent class manage this
-#ifndef DEBUG_INTEGRATION
-    [super applicationWillEnterForeground:application];
-#endif
+- (void)application:(UIApplication *)application didChangeStatusBarOrientation:(UIInterfaceOrientation)oldStatusBarOrientation
+{
+    BOOL isLandscape = UIInterfaceOrientationIsLandscape(application.statusBarOrientation);
+    SDL_VideoDevice *_this = SDL_GetVideoDevice();
+
+    if (_this && _this->num_displays > 0) {
+        SDL_DisplayMode *desktopmode = &_this->displays[0].desktop_mode;
+        SDL_DisplayMode *currentmode = &_this->displays[0].current_mode;
+
+        /* The desktop display mode should be kept in sync with the screen
+         * orientation so that updating a window's fullscreen state to
+         * SDL_WINDOW_FULLSCREEN_DESKTOP keeps the window dimensions in the
+         * correct orientation. */
+        if (isLandscape != (desktopmode->w > desktopmode->h)) {
+            int height = desktopmode->w;
+            desktopmode->w = desktopmode->h;
+            desktopmode->h = height;
+        }
+
+        /* Same deal with the current mode + SDL_GetCurrentDisplayMode. */
+        if (isLandscape != (currentmode->w > currentmode->h)) {
+            int height = currentmode->w;
+            currentmode->w = currentmode->h;
+            currentmode->h = height;
+        }
+
+        int w = (int) currentmode->w;
+        int h = (int) currentmode->h;
+
+        for (SDL_Window *window = _this->windows; window != nil; window = window->next) {
+            SDL_SendWindowEvent(window, SDL_WINDOWEVENT_RESIZED, w, h);
+        }
+    }
 }
 
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // SDL parent class manage this
-#ifndef DEBUG_INTEGRATION
-    [super applicationDidBecomeActive:application];
-#endif
+- (void)applicationWillResignActive:(UIApplication*)application
+{
+    SDL_VideoDevice *_this = SDL_GetVideoDevice();
+    if (_this) {
+        SDL_Window *window;
+        for (window = _this->windows; window != nil; window = window->next) {
+            SDL_SendWindowEvent(window, SDL_WINDOWEVENT_FOCUS_LOST, 0, 0);
+            SDL_SendWindowEvent(window, SDL_WINDOWEVENT_MINIMIZED, 0, 0);
+        }
+    }
+    SDL_SendAppEvent(SDL_APP_WILLENTERBACKGROUND);
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // SDL parent class manage this
-#ifndef DEBUG_INTEGRATION
-    [super applicationWillTerminate:application];
-#endif
+- (void)applicationDidEnterBackground:(UIApplication*)application
+{
+    SDL_SendAppEvent(SDL_APP_DIDENTERBACKGROUND);
+}
+
+- (void)applicationWillEnterForeground:(UIApplication*)application
+{
+    SDL_SendAppEvent(SDL_APP_WILLENTERFOREGROUND);
+}
+
+- (void)applicationDidBecomeActive:(UIApplication*)application
+{
+    SDL_SendAppEvent(SDL_APP_DIDENTERFOREGROUND);
+
+    SDL_VideoDevice *_this = SDL_GetVideoDevice();
+    if (_this) {
+        SDL_Window *window;
+        for (window = _this->windows; window != nil; window = window->next) {
+            SDL_SendWindowEvent(window, SDL_WINDOWEVENT_FOCUS_GAINED, 0, 0);
+            SDL_SendWindowEvent(window, SDL_WINDOWEVENT_RESTORED, 0, 0);
+        }
+    }
 }
 
 @end
 
-extern "C" {
-    int SDL_main(int argc, char *argv[])
-    {
-        return LK8000_main(argc, argv);
-    }
-}
---
+++ ios/LK8000i/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LK8000
+//
+//  Created by Nicola Ferruzzi on 23/07/2018.
+//  Copyright © 2018 Nicola Ferruzzi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
